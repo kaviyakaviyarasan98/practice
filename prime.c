158a085222dff769ec6758efacc838130ae9dc80
kaviyakaviyarasan98 <31559077+kaviyakaviyarasan98@users.noreply.github.com>
2017-09-02 15:13:39 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+
+int main(void)
+ {
+	
+int num;
+	
+int i=2;
+	
+scanf("%d",&num);
+	
+for(i=2;i<num;i++)
+	
+{
+		
+if(num%i==0)
+		
+{
+			
+printf("not a prime");
+		
+exit(0);
+	
+}
+		
+	
+}
+	
+printf("prime");
+	
+return 0;
+
+}
